9706777f0008b67be68ebb61b9634b07adde043e
itsHanibee <itshanibee@gmail.com>
2023-11-02 10:54:15 +0000

    chime: Import SD662 `msm_irqbalance.conf`
    It's been removed from the vendor a while back, the binary spits out an error because it can't find its .conf file.
    
    Signed-off-by: itsHanibee <itshanibee@gmail.com>

--- device.mk
+++ device.mk
@@ -205,6 +205,9 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# IRQ
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
