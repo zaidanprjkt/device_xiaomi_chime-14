529aab97451e843b06a82cc53459893c27268f4d
itsHanibee <itshanibee@gmail.com>
2023-11-05 23:47:36 +0530

    chime: move msm_irqbalance to vendor repo
    Signed-off-by: itsHanibee <itshanibee@gmail.com>

--- device.mk
+++ device.mk
@@ -209,9 +209,6 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
-# IRQ
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
