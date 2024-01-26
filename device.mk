3b4aae095e95c4ea375e3bd11e7b24db47a18939
hani <itshanibee@gmail.com>
2024-01-26 16:19:07 +0530

    KSUManager: good riddance
    Signed-off-by: hani <itshanibee@gmail.com>

--- device.mk
+++ device.mk
@@ -219,14 +219,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.bengal
 
-# KernelSU
-#PRODUCT_PACKAGES += \
-#    KSUManager
-
-#PRODUCT_COPY_FILES += \
-#    $(LOCAL_PATH)/KSUManager/lib/libkernelsu.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libkernelsu.so \
-#    $(LOCAL_PATH)/KSUManager/lib/libksud.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libksud.so
-
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
