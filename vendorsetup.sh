ef3c0abb77fd160772e11755bd7cb103403e6a7f
hani <itshanibee@gmail.com>
2024-05-04 11:50:14 +0530

    scripts: pull dependencies from personal namespace
    Signed-off-by: hani <itshanibee@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
 # Clone Kernel
-git clone --depth=1 https://github.com/BootleggersROM-Devices/kernel_xiaomi_chime -b android kernel/xiaomi/chime
+git clone --depth=1 https://github.com/Dominium-Apum/kernel_xiaomi_chime -b android kernel/xiaomi/chime
 
 # Clone Vendor
-git clone https://github.com/BootleggersROM-Devices/vendor_xiaomi_chime vendor/xiaomi/chime
+git clone https://github.com/Dominium-Apum/vendor_xiaomi_chime vendor/xiaomi/chime
 
 # Clone Hardware
 # rm -rf hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
